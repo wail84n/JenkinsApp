be061cfb130746ab00ca6d6472fff6f4cd21d9ba
wael al mohammad <w.almohammad@tatayab.com>
2022-12-17 23:03:11 +0300

    xsss

--- JenkinsApp/ViewController.swift
+++ JenkinsApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hello World")
+        print("Hello World111")
         // Do any additional setup after loading the view.
     }
 
