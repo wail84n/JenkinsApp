7ee4621a84c552936b09ed96ca78b76ec0acd648
wael al mohammad <w.almohammad@tatayab.com>
2022-12-24 20:36:13 +0300

    - add blus

--- JenkinsApp/ViewController.swift
+++ JenkinsApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         #if DEVELOPMENT
-        print("I am in Developmentvddcdddd")
+        print("I am in Developmentvddcd.+++")
         #elseif DEBUG
         print("I am in Debugddsdsdddfdd")
         #endif
